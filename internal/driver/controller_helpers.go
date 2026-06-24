a3b4c338ce47ce9691ce3a69ec15bb82f936d280
Khaja Omer <56000175+komer3@users.noreply.github.com>
2026-06-24 12:16:23 -0500

    [feat] Implement controller validate capabilities and get volume (#21)
    * Factor shared publish request parsing
    
    * Consolidate controller policy lookup
    
    * Implement controller validate and get volume
    
    * Address get-volume review feedback

--- internal/driver/controller_helpers.go
+++ internal/driver/controller_helpers.go
@@ -185,6 +185,26 @@ func csiVolume(filesystem *linodego.NFSFilesystem, capacityBytes int64) *csi.Vol
 	}
 }
 
+func csiControllerVolumeStatus(policy *linodego.NFSFilesystemAccessPolicy) *csi.ControllerGetVolumeResponse_VolumeStatus {
+	if !policy.Enabled {
+		return &csi.ControllerGetVolumeResponse_VolumeStatus{}
+	}
+
+	return &csi.ControllerGetVolumeResponse_VolumeStatus{PublishedNodeIds: publishedNodeIDs(policy.LinodeIDs)}
+}
+
+func publishedNodeIDs(linodeIDs []int) []string {
+	if len(linodeIDs) == 0 {
+		return nil
+	}
+
+	nodeIDs := make([]string, 0, len(linodeIDs))
+	for _, linodeID := range linodeIDs {
+		nodeIDs = append(nodeIDs, strconv.Itoa(linodeID))
+	}
+	return nodeIDs
+}
+
 func linodeError(err error, message string) error {
 	if err == nil {
 		return nil
